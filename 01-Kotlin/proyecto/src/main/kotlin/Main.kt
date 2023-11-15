8d1e34be41185e45127031836bafee018c5309fc
Nicolas Burbano <nicolas.rnbp@gmail.com>
2023-11-15 14:42:27 -0500

    some basic utils

--- 01-Kotlin/proyecto/src/main/kotlin/Main.kt
+++ 01-Kotlin/proyecto/src/main/kotlin/Main.kt
@@ -1,3 +1,33 @@
 fun main() {
     println("Hola Mundo")
+
+    val inmutable: String = "Nicolas"
+
+    var mutable: String = "Roberto"
+    mutable = "Nicolas"
+    // Duck typing asigna el tipo de manera automatica
+    var varibale = "Roberto"
+
+    // Variable primitiva
+    val nombreProfesor: String = "Adrian Eguez"
+    val sueldo: Double = 1.2
+    val estadoCivil: Char = 'C'
+    val mayorEdad: Boolean = true
+    // Clases Java
+    val fechaNacimiento: Date = Date()
+
+    val estadoCivilWhen = "C"
+    when (estadoCivilWhen) {
+        ("C") -> {
+            println("Casado")
+        }
+        ("S") -> {
+            println("Soltero")
+        }
+        else -> {
+            println("No sabemos")
+        }
+    }
+
+    val coqueteo = if (estadoCivilWhen == "S") "Si" else "No"
 }
