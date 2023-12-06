b249f7cb963db7c236fcb8cb886236d2de833659
Nicolas Burbano <nicolas.rnbp@gmail.com>
2023-12-06 15:09:15 -0500

    add snackbar

--- 02-Android/app/src/main/java/com/example/b2023_gr1sw_rnbp/ACicloVIda.kt
+++ 02-Android/app/src/main/java/com/example/b2023_gr1sw_rnbp/ACicloVIda.kt
@@ -2,8 +2,56 @@ package com.example.b2023_gr1sw_rnbp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import com.google.android.material.snackbar.Snackbar
 
-class AcicloVIda : AppCompatActivity() {
+class ACicloVIda : AppCompatActivity() {
+    var textoGlobal = ""
+    fun mostrarSnackbar(texto: String) {
+        textoGlobal = textoGlobal + " " + texto;
+        Snackbar.make(
+            findViewById(R.id.cl_aciclo_vida),
+            textoGlobal,
+            Snackbar.LENGTH_INDEFINITE
+        ).show()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aciclo_vida)
+        mostrarSnackbar("OnCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mostrarSnackbar("OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mostrarSnackbar("OnResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        mostrarSnackbar("OnRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mostrarSnackbar("OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mostrarSnackbar("OnStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mostrarSnackbar("OnDestroy")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aciclo_vida)
--- 02-Android/app/src/main/java/com/example/b2023_gr1sw_rnbp/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023_gr1sw_rnbp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.b2023_gr1sw_rnbp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        val botonCicloVida=findViewById<Button>(R.id.btn_ciclo_vida)
+        boton.CicloVida.setOnClickListener{
+            irActividad(ACicloVIda::class.java);
+        }
+    }
+    fun irActividad(
+        clase:Class<T>
+    ){
+        val itent=Intent(this,clase)
+        startActivity(intent)
     }
 }
