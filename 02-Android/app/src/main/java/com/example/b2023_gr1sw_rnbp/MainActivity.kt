704e6662fba95c1fbbb6aae3e43d22bde09347c1
Nicolas Burbano <nicolas.rnbp@gmail.com>
2023-12-20 15:59:21 -0500

    add intent

--- 02-Android/app/src/main/java/com/example/b2023_gr1sw_rnbp/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023_gr1sw_rnbp/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.b2023_gr1sw_rnbp
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,19 +17,89 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        val botonCicloVida=findViewById<Button>(R.id.btn_ciclo_vida)
-        botonCicloVida.setOnClickListener{
+        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        botonCicloVida.setOnClickListener {
             irActividad(ACicloVIda::class.java);
         }
-        val botonListView=findViewById<Button>(R.id.btn_listView)
-        botonListView.setOnClickListener{
+        val botonListView = findViewById<Button>(R.id.btn_listView)
+        botonListView.setOnClickListener {
             irActividad(BListView::class.java);
         }
+
+        val botonIntentImplicito = findViewById<Button>(
+            R.id.btn_ir_intent_implicito
+        )
+        botonIntentImplicito
+            .setOnClickListener {
+                val intentConRespuesta = Intent(
+                    Intent.ACTION_PICK,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                )
+                callbackIntentPickUri.launch(intentConRespuesta)
+            }
+        val botonIntentExplicito = findViewById<Button>(
+            R.id.btn_ir_intent_explicito
+        )
+        botonIntentExplicito
+            .setOnClickListener {
+                abrirActividadConParametros(
+                    CIntentExplicitoParametros::class.java
+                )
+            }
     }
+
+    fun abrirActividadconParametros(clase: Class<*>) {
+        val intentExplicito = Intent(this, clase)
+        // Enviar parametros (solamente variables primitivas)
+        intentExplicito.putExtra("nombre", "Nicolas")
+        intentExplicito.putExtra("apellido", "Burbano")
+        intentExplicito.putExtra("edad", 24)
+        callbackContenidoIntentExplicito.launch(intentExplicito)
+            
+    }
+
     fun irActividad(
-        clase:Class<*>
-    ){
-        val itent=Intent(this,clase)
+        clase: Class<*>
+    ) {
+        val itent = Intent(this, clase)
         startActivity(intent)
+    }
+
+    val callbackContenidoIntentExplicito = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            if (result.data != null) {
+                // Logica del negocio
+                val data = result.data
+                mostrarSnackbar("${data?.getStringExtra("nombreModificado")}")
+            }
+        }
+    }
+
+    fun mostrarSnackbar(texto: String) {
+        Snackbar.make(findViewById(R.id.id_layout_main), texto, Snackbar.LENGTH_LONG).show()
+    }
+
+    val callbackIntentPickUri = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode === Activity.RESULT_OK) {
+            if (result.data != null) {
+                if (result.data!!.data != null) {
+                    val uri: Uri = result.data!!.data!!
+                    val cursor = contentResolver.query(
+                        uri,
+                        null, null, null, null, null
+                    )
+                    cursor?.moveToFirst()
+                    val indiceTelefono =
+                        cursor?.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                    val telefono = cursor?.getString(indiceTelefono!!)
+                    cursor?.close()
+                    mostrarSnackbar("Telefono${telefono}")
+                }
+            }
+        }
     }
 }
