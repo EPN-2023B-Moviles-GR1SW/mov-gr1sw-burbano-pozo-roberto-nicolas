216bdd04175d1c5196155d76ec4547c26fbf50b5
Nicolas Burbano <rnbp@Nicolass-MacBook-Pro.local>
2024-01-16 15:39:06 -0500

    add recycler view finish

--- 02-Android/app/src/main/java/com/example/b2023_gr1sw_rnbp/FRecyclerView.kt
+++ 02-Android/app/src/main/java/com/example/b2023_gr1sw_rnbp/FRecyclerView.kt
@@ -2,10 +2,36 @@ package com.example.b2023_gr1sw_rnbp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class FRecyclerView : AppCompatActivity() {
+    var totalLikes=0
+
+    fun aumentarTotalLikes(){
+        totalLikes=totalLikes+1
+        var totalLikesTextView=findViewById<TextView>(R.id.tv_total_likes)
+        totalLikesTextView.text=totalLikes.toString()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frecycler_view)
+        inicializarRecyclerView()
+    }
+
+    fun inicializarRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_entrenadores)
+        val adaptador = FRecyclerViewAdaptadorNombreDescripcion(
+            this, // Contexto
+            BBaseDatosMemoria.arregloBEntrenador, // Arreglo datos
+            recyclerView // Recycler view
+        )
+        recyclerView.adapter = adaptador
+        recyclerView.itemAnimator = androidx.recyclerview.widget
+            .DefaultItemAnimator()
+        recyclerView.layoutManager = androidx.recyclerview.widget
+            .LinearLayoutManager(this)
+        adaptador.notifyDataSetChanged()
     }
 }
